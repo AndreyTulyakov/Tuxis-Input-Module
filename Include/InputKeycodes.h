5251e1c6ef154e2cf3be1361fe5642ff8cc49f42
Andrew Tulay <mhyhre@gmail.com>
2012-09-26 09:03:46 +0400

    Change some Keycode's

--- Include/InputKeycodes.h
+++ Include/InputKeycodes.h
@@ -12,6 +12,8 @@ namespace Tuxis
 
 	namespace Key
 	{
+		// On main keyboard
+
 		const int ESCAPE          =0x01;
 		const int _1              =0x02;
 		const int _2              =0x03;
@@ -23,9 +25,9 @@ namespace Tuxis
 		const int _8              =0x09;
 		const int _9              =0x0A;
 		const int _0              =0x0B;
-		const int MINUS           =0x0C;   /* - on main keyboard */
+		const int MINUS           =0x0C;
 		const int EQUALS          =0x0D;
-		const int BACKSPACE       =0x0E;    /* backspace */
+		const int BACKSPACE       =0x0E;
 		const int TAB             =0x0F;
 		const int Q               =0x10;
 		const int W               =0x11;
@@ -39,7 +41,7 @@ namespace Tuxis
 		const int P               =0x19;
 		const int LBRACKET        =0x1A;
 		const int RBRACKET        =0x1B;
-		const int RETURN          =0x1C;   /* Enter on main keyboard */
+		const int RETURN          =0x1C;
 		const int LCONTROL        =0x1D;
 		const int A               =0x1E;
 		const int S               =0x1F;
@@ -52,7 +54,7 @@ namespace Tuxis
 		const int L               =0x26;
 		const int SEMICOLON       =0x27;
 		const int APOSTROPHE      =0x28;
-		const int GRAVE           =0x29;   /* accent grave */
+		const int GRAVE           =0x29;
 		const int LSHIFT          =0x2A;
 		const int BACKSLASH       =0x2B;
 		const int Z               =0x2C;
@@ -63,11 +65,10 @@ namespace Tuxis
 		const int N               =0x31;
 		const int M               =0x32;
 		const int COMMA           =0x33;
-		const int PERIOD          =0x34;   /* . on main keyboard */
-		const int SLASH           =0x35;  /* / on main keyboard */
+		const int PERIOD          =0x34;
+		const int SLASH           =0x35; 
 		const int RSHIFT          =0x36;
-		const int MULTIPLY        =0x37;   /* * on numeric keypad */
-		const int LALT            =0x38;  /* left Alt */
+		const int LALT            =0x38;
 		const int SPACE           =0x39;
 		const int CAPSLOCK        =0x3A;
 		const int F1              =0x3B;
@@ -83,50 +84,59 @@ namespace Tuxis
 		const int F11             =0x57;
 		const int F12             =0x58;
 
+
 		const int NUMLOCK         =0x45;
-		const int SCROLLOCK       =0x46;   /* Scroll Lock */
+		const int SCROLLOCK       =0x46;
 
 
+		// On numeric keypad
+		const int NUMPADMULT	  =0x37;
 		const int NUMPAD7         =0x47;
 		const int NUMPAD8         =0x48;
 		const int NUMPAD9         =0x49;
-		const int NUMPADMINUS     =0x4A;   /* - on numeric keypad */
+		const int NUMPADMINUS     =0x4A; 
 		const int NUMPAD4         =0x4B;
 		const int NUMPAD5         =0x4C;
 		const int NUMPAD6         =0x4D;
-		const int NUMPADPLUS      =0x4E;   /* + on numeric keypad */
+		const int NUMPADPLUS      =0x4E;
 		const int NUMPAD1         =0x4F;
 		const int NUMPAD2         =0x50;
 		const int NUMPAD3         =0x51;
 		const int NUMPAD0         =0x52;
-		const int NUMPADPERIOD    =0x53;   /* . on numeric keypad */
-		const int NUMPADEQUALS    =0x8D;    /* = on numeric keypad (NEC PC98) */
-		const int NUMPADENTER     =0x9C;  /* Enter on numeric keypad */
-		const int NUMPADCOMMA     =0xB3;   /* , on numeric keypad (NEC PC98) */
-		const int NUMPADDIVIDE    =0xB5;  /* / on numeric keypad */
+		const int NUMPADPERIOD    =0x53;
+		const int NUMPADEQUALS    =0x8D;
+		const int NUMPADENTER     =0x9C;
+		const int NUMPADCOMMA     =0xB3;
+		const int NUMPADDIVIDE    =0xB5;
 
-		const int  UP              =0xC8;    /* UpArrow on arrow keypad */
-		const int  LEFT            =0xCB;  /* LeftArrow on arrow keypad */
-		const int  DOWN            =0xD0;  /* DownArrow on arrow keypad */
-		const int  RIGHT           =0xCD; /* RightArrow on arrow keypad */
 
-		const int  PAUSE           =0xC5;  /* Pause */
-		const int  HOME            =0xC7; /* Home on arrow keypad */
-		const int  OEM_102         =0x56;  /* <> or \| on RT 102-key keyboard (Non-U.S.) */
+		// Arrow keypad
+
+		const int  UP              =0xC8;
+		const int  LEFT            =0xCB;
+		const int  DOWN            =0xD0;
+		const int  RIGHT           =0xCD;
+
+		const int  PAUSE           =0xC5;
+		const int  HOME            =0xC7;
+		const int  OEM_102         =0x56;
 		const int  RCONTROL        =0x9D;
-		const int  RALT            =0xB8;   /* right Alt */
+		const int  RALT            =0xB8;
 		const int  SYSRQ           =0xB7;
-		const int  END             =0xCF;   /* End on arrow keypad */
-		const int  PAGEUP          =0xC9;   /* PgUp on arrow keypad */
-		const int  PAGEDOWN        =0xD1; /* PgDn on arrow keypad */
-		const int  INSERT          =0xD2;    /* Insert on arrow keypad */
-		const int  _DELETE         =0xD3;   /* Delete on arrow keypad */
-		const int  LWIN            =0xDB;  /* Left Windows key */
-		const int  RWIN            =0xDC; /* Right Windows key */
+		const int  END             =0xCF;
+		const int  PAGEUP          =0xC9;
+		const int  PAGEDOWN        =0xD1;
+		const int  INSERT          =0xD2;
+		const int  _DELETE         =0xD3;
+		const int  LWIN            =0xDB;
+		const int  RWIN            =0xDC;
+
+
+		// Mouse keys
 
 		const int MOUSE_LEFT       = 0;
 		const int MOUSE_RIGHT      = 1;
-		const int MOUSE_WHEEL      = 2;
+		const int MOUSE_WHEEL      = 2; // may middle button
 	}
 
 }
